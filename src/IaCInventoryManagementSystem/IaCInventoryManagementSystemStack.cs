8545dd3184413bb3c2b673ee4cf98713b122265c
Jhossed Molina <jhossedmolina@gmail.com>
2024-09-14 19:36:28 -0500

    Initial commit

---
+++ src/IaCInventoryManagementSystem/IaCInventoryManagementSystemStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace IaCInventoryManagementSystem
+{
+    public class IaCInventoryManagementSystemStack : Stack
+    {
+        internal IaCInventoryManagementSystemStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
